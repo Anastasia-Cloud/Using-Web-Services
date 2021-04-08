2a116574f57387dc51f12f5fba4855b084a6b6c4
AnastasiaCloud <>
2021-04-08 17:38:45 +0000

    Разбор JSON

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import xml.etree.ElementTree as ET
+import json
 
-input = '''
-<stuff>
-  <users>
-    <user x="2">
-      <id>001</id>
-      <name>Chuck</name>
-    </user>
-    <user x="7">
-      <id>009</id>
-      <name>Brent</name>
-    </user>
-  </users>
-</stuff>'''
+data = '''
+[
+  { "id" : "001",
+    "x" : "2",
+    "name" : "Chuck"
+  } ,
+  { "id" : "009",
+    "x" : "7",
+    "name" : "Brent"
+  }
+]'''
 
-stuff = ET.fromstring(input)
-lst = stuff.findall('users/user')
-print('User count:', len(lst))
+info = json.loads(data)
+print('User count:', len(info))
 
-for item in lst:
-    print('Name', item.find('name').text)
-    print('Id', item.find('id').text)
-    print('Attribute', item.get('x'))
+for item in info:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
